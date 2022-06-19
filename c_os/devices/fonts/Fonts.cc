a18636ce87923f896365f0b3b0c8d2b515dfbd74
churl <tobi@urpost.de>
2022-06-19 13:23:54 +0200

    reformat

--- c_os/devices/fonts/Fonts.cc
+++ c_os/devices/fonts/Fonts.cc
@@ -11,9 +11,9 @@
 #include "devices/fonts/Font_sun_12x22.h"
 #include "devices/fonts/Font_sun_8x16.h"
 
-Font_8x16	       std_font_8x16;
-Font_8x8	       std_font_8x8;
-Font_acorn_8x8	   acorn_font_8x8;
-Font_pearl_8x8	   pearl_font_8x8;
-Font_sun_12x22	   sun_font_12x22;
-Font_sun_8x16	   sun_font_8x16;
+Font_8x16 std_font_8x16;
+Font_8x8 std_font_8x8;
+Font_acorn_8x8 acorn_font_8x8;
+Font_pearl_8x8 pearl_font_8x8;
+Font_sun_12x22 sun_font_12x22;
+Font_sun_8x16 sun_font_8x16;
