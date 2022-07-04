d2e6a9f9b9fe489f53319b56bdd3b32e19395b54
ChUrl <christoph.urlacher@protonmail.com>
2022-07-04 11:23:15 +0200

    Init flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A basic flake with a shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          # Native inputs run on host
+          nativeBuildInputs = with pkgs; [
+            bashInteractive
+            gnumake
+            gcc
+            nasm
+          ];
+
+          # Build inputs are for target platform, app will be linked against those
+          buildInputs = with pkgs; [ ];
+        };
+      });
+}
