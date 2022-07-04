2d2eec6934274af573a062a38e26173cbda484b7
ChUrl <christoph.urlacher@protonmail.com>
2022-07-04 22:06:19 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -19,13 +19,7 @@
             qemu
             gdb
             bear # To generate compilation database
-
-            # TODO: Get clang tooling to work, in combination with fcc it can't find standard headers
-            # llvmPackages_14.clang-unwrapped
-            # clang_14 # LSP + formatting
-            # libcxxStdenv
             clang-tools_14
-            # llvmPackages.libcxxClang # LSP + formatting
           ];
 
           # Build inputs are for target platform, app will be linked against those
