42a1d719eeb38bf03c2772a98d9a0e30cb5989d4
ChUrl <christoph.urlacher@protonmail.com>
2022-07-04 16:20:46 +0200

    disable forced bluescreen

--- c_os/main.cc
+++ c_os/main.cc
@@ -52,23 +52,22 @@ int main() {
 
     // Activate paging
     // This has to happen after the allocator is initialized but before the scheduler is started
-    // TODO: Scheduler triggers bluescreen?
     pg_init();
 
     // Trigger Bluescreen
-    kout << "Trigger Bluescreen, if you can read this it didn't work" << endl;
+    // kout << "Trigger Bluescreen, if you can read this it didn't work" << endl;
 
     // BlueScreen 1
     // asm("int $3");
 
     // BlueScreen 2
-    unsigned int* page = pg_alloc_page();
-    *page = 42;
-    pg_write_protect_page(page);
-    invalidate_tlb_entry(page); // If we don't invalidate after first access the write protection
-                                // won't work as no lookup is performed (address in tlb)
-    *page = 42; // We map logical to physical 1:1 so no need to do any lookup
-                // If tlb is invalidated this access produces a pagefault
+    // unsigned int* page = pg_alloc_page();
+    // *page = 42;
+    // pg_write_protect_page(page);
+    // invalidate_tlb_entry(page); // If we don't invalidate after first access the write protection
+    //                             // won't work as no lookup is performed (address in tlb)
+    // *page = 42; // We map logical to physical 1:1 so no need to do any lookup
+    //             // If tlb is invalidated this access produces a pagefault
 
     // Demo threads anlegen
     // scheduler.ready(new HelloWorldThread());
