25da4b8596e7d968bbe47d36fc3fd8dea9362477
ChUrl <christoph.urlacher@protonmail.com>
2022-07-11 19:22:23 +0200

    update preemption demo

--- c_os/user/demo/PreemptiveThreadDemo.cc
+++ c_os/user/demo/PreemptiveThreadDemo.cc
@@ -1,11 +1,23 @@
-#include "user/PreemptiveThreadDemo.h"
-#include "lib/Semaphore.h"
+#include "user/demo/PreemptiveThreadDemo.h"
+
+void PreemptiveLoopThread::run() {
+    int cnt = 0;
+    while (true) {
+        // Basic synchronization by semaphore
+        sem->p();
+
+        kout.setpos(55, this->id);
+        kout << this->id << ": " << dec << cnt++ << endl;
+
+        sem->v();
+    }
+}
 
 void PreemptiveThreadDemo::run() {
     kout << "PreemptiveThreadDemo ====================================" << endl;
     kout << "Initializing Semaphore" << endl;
-    Semaphore* sem = new Semaphore(1); // Create this semaphore on the heap as this thread exits itself,
-                                       // so stack allocated objects will be lost
+    Semaphore* sem = new Semaphore(1);  // Create this semaphore on the heap as this thread exits itself,
+                                        // so stack allocated objects will be lost
 
     kout << "Allocating LoopThread A" << endl;
     Thread* cntA = new PreemptiveLoopThread(0, sem);
--- c_os/user/demo/PreemptiveThreadDemo.h
+++ c_os/user/demo/PreemptiveThreadDemo.h
@@ -1,12 +1,11 @@
-#ifndef __pre_loopthread_include__
-#define __pre_loopthread_include__
+#ifndef __preemptive_thread_include__
+#define __preemptive_thread_include__
 
 #include "kernel/Globals.h"
 #include "kernel/threads/Thread.h"
 #include "lib/Semaphore.h"
 
 class PreemptiveLoopThread : public Thread {
-
 private:
     int id;
     Semaphore* sem;
@@ -18,6 +17,19 @@ public:
     PreemptiveLoopThread(int i, Semaphore* sem) : id(i), sem(sem) {}
 
     // Zaehlt einen Zaehler hoch und gibt ihn auf dem Bildschirm aus.
+    void run() override;
+};
+
+class PreemptiveThreadDemo : public Thread {
+private:
+    PreemptiveThreadDemo(const PreemptiveThreadDemo& copy) = delete;  // Verhindere Kopieren
+
+public:
+    PreemptiveThreadDemo() {
+        kout << "Initialized PreemptiveThreadDemo" << endl;
+    }
+
+    // Thread-Startmethode
     void run() override;
 };
 
