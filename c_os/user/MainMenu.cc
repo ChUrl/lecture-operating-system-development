6c0fff0b6c34d521db83acf96a0474781c5b22f4
ChUrl <christoph.urlacher@protonmail.com>
2022-07-16 01:03:13 +0200

    start demos from mainmenu

--- c_os/user/MainMenu.cc
+++ c_os/user/MainMenu.cc
@@ -1,7 +1,27 @@
 #include "user/MainMenu.h"
+#include "user/demo/ArrayListDemo.h"
+#include "user/demo/BlueScreenDemo.h"
+#include "user/demo/HeapDemo.h"
+#include "user/demo/KeyboardDemo.h"
+#include "user/demo/PCSPKdemo.h"
+#include "user/demo/PreemptiveThreadDemo.h"
+#include "user/demo/TextDemo.h"
+#include "user/demo/VBEdemo.h"
 
 void MainMenu::run() {
     kout << "Main Menu" << endl;
+
+    // Demos
+    // scheduler.ready(new TextDemo());
+    // scheduler.ready(new PCSPKdemo(&PCSPK::aerodynamic));
+    // scheduler.ready(new KeyboardDemo());
+    // scheduler.ready(new HeapDemo());
+    // scheduler.ready(new VBEdemo());
+    // scheduler.ready(new BlueScreenDemo());
+    scheduler.ready(new PreemptiveThreadDemo(3));
+
+    // Extra demos
+    // scheduler.ready(new ArrayListDemo());
 
     scheduler.exit();
 }
--- c_os/user/MainMenu.h
+++ c_os/user/MainMenu.h
@@ -1,16 +1,20 @@
 #ifndef __MainMenu_Inlucde_H_
 #define __MainMenu_Inlucde_H_
 
-#include "kernel/threads/Thread.h"
 #include "kernel/Globals.h"
+#include "kernel/threads/Thread.h"
+#include "user/event/KeyEventListener.h"
 
 class MainMenu : public Thread {
 private:
     MainMenu(const MainMenu& copy) = delete;
 
+    KeyEventListener listener;
+
 public:
-    MainMenu() {
+    MainMenu() : listener(*this) {
         kout << "MainMenu initialized" << endl;
+        // kevman.subscribe(this->listener);
     }
 
     void run() override;
