75a77a83949e5d4b0f0b105b742e59b789dac29d
ChUrl <christoph.urlacher@protonmail.com>
2022-07-16 20:13:41 +0200

    reformat

--- c_os/user/event/KeyEventListener.h
+++ c_os/user/event/KeyEventListener.h
@@ -10,12 +10,12 @@ private:
     char lastChar;
 
 public:
-    Thread& thread; // Thread which contains this listener, so the listener can block the thread
+    Thread& thread;  // Thread which contains this listener, so the listener can block the thread
 
     KeyEventListener(Thread& thread) : thread(thread) {}
 
-    char waitForKeyEvent() const; // Blocks the thread until woken up by manager
-    void trigger(char c); // Gets called from KeyEventManager
+    char waitForKeyEvent() const;  // Blocks the thread until woken up by manager
+    void trigger(char c);          // Gets called from KeyEventManager
 };
 
 #endif
