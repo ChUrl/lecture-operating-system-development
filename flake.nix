cf4a51b47a23a65d2fdc1dcf0c5624fa760cc779
ChUrl <christoph.urlacher@protonmail.com>
2022-07-22 18:11:56 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -13,19 +13,34 @@
           # Native inputs run on host
           nativeBuildInputs = with pkgs; [
             gcc_multi # Compile with 32bit
-            glibc_multi
+            glibc_multi # Needed for lsp to find some headers (can cause compilation errors when trying to
+                        # compile some file standalone)
             nasm
             binutils
+
             gnumake
             bear # To generate compilation database
             gdb
             qemu # Start os in virtual machine
-            clang-tools_14 # Editor LSP
+
+            clang-tools_14 # clangd + clang-format + clang-tidy, the counterparts included in clang_14 package
+                           # don't function correctly somehow (don't find headers, probably some conflict between
+                           # nix clang and gcc environments)
+            clang_14 # To view template generation, also alternative error messages
+
+            # TODO: Figure out what is needed to make cling work
+            # llvmPackages_14.llvm
             # cling # To try out my bullshit implementations
+            # root
           ];
 
           # Build inputs are for target platform, app will be linked against those
           buildInputs = with pkgs; [ ];
+
+          # shellHook = ''
+          #   alias makeg="CC=gcc CXX=g++ make -j 8"
+          #   alias makec="CC=clang CXX=clang++ make -j 8"
+          # '';
         };
       });
 }
