cde3967961ca15a35aeaa85487c2d72125371b8a
ChUrl <christoph.urlacher@protonmail.com>
2022-07-22 18:13:30 +0200

    fix semaphore bug (preempt before block/deblock)

--- c_os/lib/Semaphore.cc
+++ c_os/lib/Semaphore.cc
@@ -2,7 +2,7 @@
 #include "kernel/Globals.h"
 
 void Semaphore::p() {
-    // Lock to allow deterministic operations on counter
+    // Lock to allow deterministic operations on counter/queue
     this->lock.acquire();
 
     if (this->counter > 0) {
@@ -12,8 +12,10 @@ void Semaphore::p() {
     } else {
         // Block and manage thread in semaphore queue until it's woken up by v() again
         this->wait_queue.push_back(scheduler.get_active());
+
+        cpu.disable_int();  // Make sure the block() comes through after releasing the lock
         this->lock.release();
-        scheduler.block();  // Moves to next thread
+        scheduler.block();  // Moves to next thread, enables int
     }
 }
 
@@ -22,14 +24,12 @@ void Semaphore::v() {
 
     if (!this->wait_queue.empty()) {
         // Semaphore stays busy and unblocks next thread to work in critical section
-        if (this->wait_queue.empty()) {
-            this->lock.release();
-            return;
-        }
         unsigned int tid = this->wait_queue.front();
         this->wait_queue.erase(wait_queue.begin());
+
+        cpu.disable_int();  // Make sure the deblock() comes through after releasing the lock
         this->lock.release();
-        scheduler.deblock(tid);
+        scheduler.deblock(tid);  // Enables int
     } else {
         // No more threads want to work so free semaphore
         this->counter = this->counter + 1;
